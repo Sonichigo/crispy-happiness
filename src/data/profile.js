2ade1cdbc653b9f5fa136eddcd7eca89d4a6e9a2
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2022-12-05 14:42:42 +0530

    New

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,8 +1,8 @@
 const profile = {
-  name: "Admin",
+  name: "Aniruddha Saha",
   img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample4.jpg",
   bio: "I solemnly swear that I am upto no good!",
-  points: "80",
+  points: "100",
 };
 
 export default profile;
